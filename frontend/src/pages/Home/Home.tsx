3d5e301923abcccee1da350db27584f0ef3a849f
Thanusree-G <thanusreegurram@gmail.com>
2026-07-08 23:35:18 +0530

    Create React project and initial home page

---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>🎮 Welcome to BattleVerse AI</h1>
+      <p>Next Generation Multiplayer Battle Arena</p>
+    </div>
+  );
+}
+
+export default Home;
